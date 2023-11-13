379980394572c268ff8edd9b3bbf890c015dd658
Marcin <marcin.karbowniczyn@gmail.com>
2023-11-13 11:57:22 +0100

    Render the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas  # Third party library to read CSV data. It's already installed with streamlit.
 
 st.set_page_config(layout="wide")
 
@@ -19,6 +20,17 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+df = pandas.read_csv('data.csv', sep=';')
+
+col3, col4 = st.columns(2)
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
 
 # import streamlit as st
 #
