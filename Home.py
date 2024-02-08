ea566111e7b94fac8c2e568cb46bd74b4f05b986
Marcin <marcin.karbowniczyn@gmail.com>
2024-02-08 16:21:22 +0100

    Add a function to hide image full screen buttons from the webpage, change the way projects are being displayed

--- Home.py
+++ Home.py
@@ -1,11 +1,15 @@
 # Z tego co rozumiem, pandas bierze dane CSV, tworzy z tego Data Frame, czyli dict type object który zawiera x rowów i y kolumn.
 import streamlit as st
 import pandas  # Third party library to read CSV data. It's already installed with streamlit.
+from utils.hide_fulscreen_button import hide_img_fs
 
 st.set_page_config(layout="wide",
                    page_title="Portfolio")
-project_image_width = 500
 
+# Function to hide fulscreen image buttons from a site
+st.markdown(hide_img_fs(), unsafe_allow_html=True)
+
+project_image_width = 500
 empty_col_intro_left, col1, col2, empty_col_intro_right = st.columns([1, 1, 1, 1])
 
 with col1:
@@ -25,15 +29,17 @@ st.divider()
 df = pandas.read_csv('data.csv', sep=';')
 empty_col_projects_left, col3, col4, empty_col_projects_right = st.columns([1, 1, 1, 1])
 with col3:
-    for index, row in df[:10].iterrows():
-        st.header(row['title'])
-        st.write(row['description'])
-        st.image('images/' + row['image'])
-        st.write(f"[Source Code]({row['url']})")
+    for index, row in df.iterrows():
+        if not int(index) % 2:
+            st.header(row['title'])
+            st.write(row['description'])
+            st.image('images/' + row['image'])
+            st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
-        st.header(row['title'])
-        st.write(row['description'])
-        st.image('images/' + row['image'])
-        st.write(f"[Source Code]({row['url']})")
+    for index, row in df.iterrows():
+        if int(index) % 2:
+            st.header(row['title'])
+            st.write(row['description'])
+            st.image('images/' + row['image'])
+            st.write(f"[Source Code]({row['url']})")
---
+++ utils/hide_fulscreen_button.py
@@ -0,0 +1,7 @@
+def hide_img_fs():
+    return '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
